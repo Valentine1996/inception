23d66f54a6529a05638691dac42ccfeac653db4a
valentyn <valentunnamisnuk@gmail.com>
2017-06-21 12:36:01 +0300

    Add tabs

---
+++ js/engine.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+        $("#singleImageCarousel").owlCarousel({
+            items: 1,
+            singleItem: true,
+            loop: true
+        });
+    
+        $("#forthImageCarousel").owlCarousel({
+            nav: true,
+            navText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
+            items: 4,
+            dots: false
+        });
+    }
+);
+
+/*
+    Function for opening tab content
+ */
+function openTabContent(evt, tabId) {
+
+
+    //Remove active tab-content
+    $(".tabcontent.content-active").removeClass("content-active");
+
+    //
+    $(".tabcontent").each( function () {
+        $(this).addClass("content-hidden");
+    });
+
+    $(".tablinks.link-active").removeClass("link-active");
+
+
+    $("#" + tabId).addClass("content-active").removeClass("content-hidden");
+    evt.currentTarget.className +=" link-active";
+}
+
+
